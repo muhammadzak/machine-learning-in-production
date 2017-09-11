6c5f82a475e7801c51e693c57bcc28627f3d9bf0
muhammadzak <31339586+muhammadzak@users.noreply.github.com>
2017-09-11 23:25:31 +0530

    Create tensorflow_installation.sh

---
+++ dl/TensorFlow/tensorflow_installation.sh
@@ -0,0 +1,11 @@
+sudo easy_install pip
+sudo pip install --upgrade virtualenv
+virtualenv --system-site-packages -p python3 tensorflow # for Python 3.n
+source ~/tensorflow/bin/activate  
+source ~/tensorflow/bin/activate.csh 
+easy_install -U pip
+pip3 install --upgrade tensorflow
+pip3 install --upgrade
+pip3 install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.3.0-py2-none-any.whl
+#deactivate
+rm -r ~/tensorflow 
